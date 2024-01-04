6442f589643d27466496c208c28ac4037c7fe190
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2024-01-04 17:44:21 +0100

    Added in db the db dump to use with mysql (created with mysqldump)

---
+++ database/letturepremiate_dump.sql
@@ -0,0 +1,365 @@
+-- MariaDB dump 10.19  Distrib 10.4.28-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: letturepremiate
+-- ------------------------------------------------------
+-- Server version	10.4.28-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `autore`
+--
+
+DROP TABLE IF EXISTS `autore`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `autore` (
+  `id` int(8) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ID_AUTORE_IND` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `autore`
+--
+
+LOCK TABLES `autore` WRITE;
+/*!40000 ALTER TABLE `autore` DISABLE KEYS */;
+/*!40000 ALTER TABLE `autore` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `commenti`
+--
+
+DROP TABLE IF EXISTS `commenti`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `commenti` (
+  `utenteIdPost` int(8) NOT NULL,
+  `dataOraPost` date NOT NULL,
+  `utenteIdComm` int(8) NOT NULL,
+  `dataOraComm` date NOT NULL,
+  `commento` varchar(1023) NOT NULL,
+  PRIMARY KEY (`utenteIdPost`,`dataOraPost`,`utenteIdComm`,`dataOraComm`),
+  UNIQUE KEY `ID_COMMENTI_IND` (`utenteIdPost`,`dataOraPost`,`utenteIdComm`,`dataOraComm`),
+  KEY `REF_COMME_UTENT_IND` (`utenteIdComm`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `commenti`
+--
+
+LOCK TABLES `commenti` WRITE;
+/*!40000 ALTER TABLE `commenti` DISABLE KEYS */;
+/*!40000 ALTER TABLE `commenti` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `compone`
+--
+
+DROP TABLE IF EXISTS `compone`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `compone` (
+  `medagliereId` int(8) NOT NULL,
+  `libroId` int(8) NOT NULL,
+  PRIMARY KEY (`libroId`,`medagliereId`),
+  UNIQUE KEY `ID_Compone_IND` (`libroId`,`medagliereId`),
+  KEY `EQU_Compo_MEDAG_IND` (`medagliereId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `compone`
+--
+
+LOCK TABLES `compone` WRITE;
+/*!40000 ALTER TABLE `compone` DISABLE KEYS */;
+/*!40000 ALTER TABLE `compone` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `libro`
+--
+
+DROP TABLE IF EXISTS `libro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `libro` (
+  `id` int(8) NOT NULL,
+  `titolo` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ID_LIBRO_IND` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `libro`
+--
+
+LOCK TABLES `libro` WRITE;
+/*!40000 ALTER TABLE `libro` DISABLE KEYS */;
+/*!40000 ALTER TABLE `libro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `medagliere`
+--
+
+DROP TABLE IF EXISTS `medagliere`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `medagliere` (
+  `id` int(8) NOT NULL,
+  `titolo` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ID_MEDAGLIERE_IND` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medagliere`
+--
+
+LOCK TABLES `medagliere` WRITE;
+/*!40000 ALTER TABLE `medagliere` DISABLE KEYS */;
+/*!40000 ALTER TABLE `medagliere` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notifica`
+--
+
+DROP TABLE IF EXISTS `notifica`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `notifica` (
+  `id` int(8) NOT NULL,
+  `dataOra` date NOT NULL,
+  `descrizione` varchar(255) NOT NULL,
+  `tipo` char(1) NOT NULL,
+  `utenteId` char(1) NOT NULL,
+  `utenteIdPost` char(1) DEFAULT NULL,
+  `dataOraPost` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ID_NOTIFICA_IND` (`id`),
+  KEY `REF_NOTIF_UTENT_IND` (`utenteId`),
+  KEY `REF_NOTIF_POST_IND` (`utenteIdPost`,`dataOraPost`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notifica`
+--
+
+LOCK TABLES `notifica` WRITE;
+/*!40000 ALTER TABLE `notifica` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notifica` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `post` (
+  `utenteId` int(8) NOT NULL,
+  `dataOra` date NOT NULL,
+  `citazioneTestuale` varchar(255) DEFAULT NULL,
+  `fotoCitazione` varchar(127) DEFAULT NULL,
+  `riflessione` varchar(2047) NOT NULL,
+  `counterMiPiace` decimal(6,0) NOT NULL,
+  `counterAdoro` decimal(6,0) NOT NULL,
+  `libroId` int(8) NOT NULL,
+  PRIMARY KEY (`utenteId`,`dataOra`),
+  UNIQUE KEY `ID_POST_IND` (`utenteId`,`dataOra`),
+  KEY `REF_POST_LIBRO_IND` (`libroId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post`
+--
+
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `scrittoda`
+--
+
+DROP TABLE IF EXISTS `scrittoda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `scrittoda` (
+  `libroId` int(8) NOT NULL,
+  `autoreId` int(8) NOT NULL,
+  PRIMARY KEY (`autoreId`,`libroId`),
+  UNIQUE KEY `ID_ScrittoDa_IND` (`autoreId`,`libroId`),
+  KEY `EQU_Scrit_LIBRO_IND` (`libroId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `scrittoda`
+--
+
+LOCK TABLES `scrittoda` WRITE;
+/*!40000 ALTER TABLE `scrittoda` DISABLE KEYS */;
+/*!40000 ALTER TABLE `scrittoda` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `segue`
+--
+
+DROP TABLE IF EXISTS `segue`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `segue` (
+  `seguitoId` char(1) NOT NULL,
+  `seguenteId` char(1) NOT NULL,
+  PRIMARY KEY (`seguitoId`,`seguenteId`),
+  UNIQUE KEY `ID_Segue_IND` (`seguitoId`,`seguenteId`),
+  KEY `REF_Segue_UTENT_1_IND` (`seguenteId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `segue`
+--
+
+LOCK TABLES `segue` WRITE;
+/*!40000 ALTER TABLE `segue` DISABLE KEYS */;
+/*!40000 ALTER TABLE `segue` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sottoscrive`
+--
+
+DROP TABLE IF EXISTS `sottoscrive`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sottoscrive` (
+  `utenteId` char(1) NOT NULL,
+  `medagliereId` int(8) NOT NULL,
+  PRIMARY KEY (`medagliereId`,`utenteId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sottoscrive`
+--
+
+LOCK TABLES `sottoscrive` WRITE;
+/*!40000 ALTER TABLE `sottoscrive` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sottoscrive` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tagperpost`
+--
+
+DROP TABLE IF EXISTS `tagperpost`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tagperpost` (
+  `utenteIdPost` char(1) NOT NULL,
+  `dataOraPost` date NOT NULL,
+  `tagId` char(1) NOT NULL,
+  PRIMARY KEY (`utenteIdPost`,`dataOraPost`,`tagId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tagperpost`
+--
+
+LOCK TABLES `tagperpost` WRITE;
+/*!40000 ALTER TABLE `tagperpost` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tagperpost` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tags` (
+  `id` int(8) NOT NULL,
+  `testo` char(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tags`
+--
+
+LOCK TABLES `tags` WRITE;
+/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `utente`
+--
+
+DROP TABLE IF EXISTS `utente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `utente` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `username` varchar(80) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `pwd` varchar(120) NOT NULL,
+  `immagineProfilo` varchar(255) NOT NULL,
+  `isAdmin` char(1) NOT NULL,
+  `descrizione` varchar(150) DEFAULT NULL,
+  `stato` char(1) NOT NULL,
+  `numeroFollow` int(8) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utente`
+--
+
+LOCK TABLES `utente` WRITE;
+/*!40000 ALTER TABLE `utente` DISABLE KEYS */;
+INSERT INTO `utente` VALUES (1,'pier','pier@tecweb.com','$2y$10$gFrPumcEaL5CcXvtGXM9xuMJXNf/UQ01qwAzFXR5XPwsPXbtJV.B6','','',NULL,'',0),(2,'luca','luca@webtec.it','$2y$10$YU5cUJ4u1mijevPHw6/eA.HpBmhiy.V6b/GQ7Ft9XaYWsIuZKW7He','','',NULL,'',0),(3,'jacopo','jacopo@webtec.it','$2y$10$42RzrrImDVkkEM3LsluT0ekx6hGZPWod7kTXa/S6.9dhct8TcxskS','','',NULL,'',0);
+/*!40000 ALTER TABLE `utente` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-01-04 17:38:32
