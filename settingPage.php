a43b130613c2780fc1c66ea90b0eea0fae956e25
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2024-01-09 10:38:32 +0100

    settingPage: beginning defining option panels to modify DB fields (JS)

--- settingPage.php
+++ settingPage.php
@@ -37,19 +37,53 @@ header("Location: index.html");
         </div>
         <div class="col-md-4 text-center">
           <ul class="list-unstyled">
-            <li><a href="#">Cambia password</a></li>
-            <li><a href="#">Modifica la tua descrizione utente</a></li>
-            <li><a href="#">Cambia immagine del profilo</a></li>
-            <li><a href="#">Elimina account</a></li>
+            <li><a href="#" onclick="showContent('password')">Cambia password</a></li>
+            <li><a href="#" onclick="showContent('description')">Modifica la tua descrizione utente</a></li>
+            <li><a href="#" onclick="showContent('profile')">Cambia immagine del profilo</a></li>
+            <li><a href="#" onclick="showContent('delete')">Elimina account</a></li>
           </ul>
         </div>
         <div class="col-md-4">
         </div>
+            <!-- Content for "Cambia password" -->
+            <div id="passwordContent" style="display: none;">
+                <!-- Content for changing password -->
+                <p>PWD</p>
+            </div>
+
+            <!-- Content for "Modifica la tua descrizione utente" -->
+            <div id="descriptionContent" style="display: none;">
+                <!-- Content for modifying user description -->
+                <p>User descr</p>
+            </div>
+
+            <!-- Content for "Cambia immagine del profilo" -->
+            <div id="profileContent" style="display: none;">
+                <!-- Content for changing profile image -->
+                <p>Profile content</p>
+            </div>
+
+            <!-- Content for "Elimina account" -->
+            <div id="deleteContent" style="display: none;">
+                <!-- Content for deleting account -->
+                <p>delete user</p>
+            </div>
       </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
     crossorigin="anonymous">
+    </script>
+    <script>
+      function showContent(contentType) {
+          document.getElementById('passwordContent').style.display = 'none';
+          document.getElementById('descriptionContent').style.display = 'none';
+          document.getElementById('profileContent').style.display = 'none';
+          document.getElementById('deleteContent').style.display = 'none';
+
+          // Show the selected content section
+          document.getElementById(contentType + 'Content').style.display = 'block';
+      }
     </script>
   </body>
 </html>
