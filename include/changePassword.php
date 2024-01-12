15c294a01af0fb876f161c50434822995a671a53
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2024-01-12 14:38:04 +0100

    changePassword.php: formatting

--- include/changePassword.php
+++ include/changePassword.php
@@ -1,36 +1,40 @@
 <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-body">
-                        <form action="updatePassword.php" method="post">
-                            <div class="form-group row">
-                                <label class="col-sm-5 col-form-label col-form-label-sm text-end" for="oldPassword">Vecchia Password:</label>
-                                <div class="col-sm-7">
-                                    <input class="form-control" type="password" id="oldPassword" name="oldPassword" required>
-                                </div>
-                                </div>
-                            <br />
-                            <div class="form-group row">
-                                <label class="col-sm-5 col-form-label col-form-label-sm text-end" for="password">Nuova Password:</label>
-                                <div class="col-sm-7">
-                                    <input class="form-control" type="password" id="password" name="password" required>
-                                </div>
-                                </div>
-                            <br />
-                            <div class="form-group row">
-                                <label class="col-sm-5 col-form-label col-form-label-sm text-end" for="confirm_password">Conferma Nuova Password:</label>
-                                <div class="col-sm-7">
-                                    <input class="form-control" type="password" id="confirm_password" name="confirm_password" required>
-                                </div>
-                                </div>
-                            <br />
-                            <div class="form-group row">
-                                <div class="col-sm-12 text-center">
-                                    <input class="btn btn-primary" type="submit" value="Aggiorna Password">
-                                </div>
-                                </div>
-                        </form>
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-body">
+                <form action="updatePassword.php" method="post">
+                    <div class="form-group row">
+                        <label class="col-sm-5 col-form-label col-form-label-sm text-end" for="oldPassword">Vecchia
+                            Password:</label>
+                        <div class="col-sm-7">
+                            <input class="form-control" type="password" id="oldPassword" name="oldPassword" required>
+                        </div>
                     </div>
-                </div>
+                    <br />
+                    <div class="form-group row">
+                        <label class="col-sm-5 col-form-label col-form-label-sm text-end" for="password">Nuova
+                            Password:</label>
+                        <div class="col-sm-7">
+                            <input class="form-control" type="password" id="password" name="password" required>
+                        </div>
+                    </div>
+                    <br />
+                    <div class="form-group row">
+                        <label class="col-sm-5 col-form-label col-form-label-sm text-end"
+                            for="confirm_password">Conferma Nuova Password:</label>
+                        <div class="col-sm-7">
+                            <input class="form-control" type="password" id="confirm_password" name="confirm_password"
+                                required>
+                        </div>
+                    </div>
+                    <br />
+                    <div class="form-group row">
+                        <div class="col-sm-12 text-center">
+                            <input class="btn btn-primary" type="submit" value="Aggiorna Password">
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
+    </div>
+</div>
