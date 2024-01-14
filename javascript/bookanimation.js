dbed6563747a5849096454ac0270f8121a7f030c
Jacopo Pesaresi <jacopo.pesaresi2@studio.unibo.it>
2024-01-14 18:56:16 +0100

    piccolo gioco sullo sfoglio del libro (provare per credere)

--- javascript/bookanimation.js
+++ javascript/bookanimation.js
@@ -1,3 +1,10 @@
+const music = new Audio('audio/sfoglia.mp3');
+music.playbackRate=4;
+// const music2 = new Audio();
+// //music.loop = true;
+// //music.playbackRate = 2;
+// //music.pause();
+
 const flipBook = (elBook) => {
     elBook.style.setProperty("--c", 0); // Set current page
 
@@ -6,6 +13,7 @@ const flipBook = (elBook) => {
         page.addEventListener("click", (evt) => {
             const curr = evt.target.closest(".back") ? idx : idx + 1;
             elBook.style.setProperty("--c", curr);
+            music.play();
         });
     });
 };
