05064a166ce68a8b152d65381cdf2d1682711682
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2024-01-16 22:28:01 +0100

    graphical fine tuning

--- include/changePassword.php
+++ include/changePassword.php
@@ -1,40 +1,45 @@
 <div class="row justify-content-center">
-    <div class="col-md-6">
-        <div class="card">
-            <div class="card-body">
-                <form action="include/updatePassword.php" method="post">
-                    <div class="form-group row">
-                        <label class="col-sm-5 col-form-label col-form-label-sm text-end" for="oldPassword">Vecchia
-                            Password:</label>
-                        <div class="col-sm-7">
-                            <input class="form-control" type="password" id="oldPassword" name="oldPassword" required>
-                        </div>
+
+    <div class="card">
+        <!-- <p>According to <cite>The Art of War</cite> by Sun Tzu, "Opportunities multiply as they are seized."</p>
+        <blockquote class="blockquote">
+            <p>The only way to do great work is to love what you do.</p>
+            <footer class="blockquote-footer">Steve Jobs</footer>
+        </blockquote> -->
+        <div class="card-body">
+            <form action="include/updatePassword.php" method="post">
+                <div class="form-group row">
+                    <label class="col-sm-5 col-form-label col-form-label-sm text-end" for="oldPassword">Vecchia
+                        Password:</label>
+                    <div class="col-sm-7">
+                        <input class="form-control" type="password" id="oldPassword" name="oldPassword" required>
                     </div>
-                    <br />
-                    <div class="form-group row">
-                        <label class="col-sm-5 col-form-label col-form-label-sm text-end" for="password">Nuova
-                            Password:</label>
-                        <div class="col-sm-7">
-                            <input class="form-control" type="password" id="password" name="password" required>
-                        </div>
+                </div>
+                <br />
+                <div class="form-group row">
+                    <label class="col-sm-5 col-form-label col-form-label-sm text-end" for="password">Nuova
+                        Password:</label>
+                    <div class="col-sm-7">
+                        <input class="form-control" type="password" id="password" name="password" required>
                     </div>
-                    <br />
-                    <div class="form-group row">
-                        <label class="col-sm-5 col-form-label col-form-label-sm text-end"
-                            for="confirm_password">Conferma Nuova Password:</label>
-                        <div class="col-sm-7">
-                            <input class="form-control" type="password" id="confirm_password" name="confirm_password"
-                                required>
-                        </div>
+                </div>
+                <br />
+                <div class="form-group row">
+                    <label class="col-sm-5 col-form-label col-form-label-sm text-end" for="confirm_password">Conferma
+                        Nuova Password:</label>
+                    <div class="col-sm-7">
+                        <input class="form-control" type="password" id="confirm_password" name="confirm_password"
+                            required>
                     </div>
-                    <br />
-                    <div class="form-group row">
-                        <div class="col-sm-12 text-center">
-                            <input class="btn btn-primary" type="submit" value="Aggiorna Password">
-                        </div>
+                </div>
+                <br />
+                <div class="form-group row">
+                    <div class="col-sm-12 text-center">
+                        <input class="btn btn-primary" type="submit" value="Aggiorna Password">
                     </div>
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
     </div>
+
 </div>
--- include/changeUserDescription.php
+++ include/changeUserDescription.php
@@ -4,15 +4,15 @@ require_once('login.model.php');
 
 <div class="card">
     <div class="row justify-content-center">
-        <div class="col-md-6">
+        <div class="col-md-10">
 
             <form action="include/updateUserDescription.php" method="post">
 
-                <div class="form-group">
+                <div class="form-group" align-items-center>
                     <label for="userDescription">Descrizione Utente</label>
-                    <textarea id="userDescription"
+                    <textarea id="userDescription" class="col-md-12"
                         name="userDescription"><?php echo getUserDescription($_SESSION['username']); ?></textarea>
-                    <!-- You can add a submit button and update.php to handle form submission -->
+
                 </div>
                 <div class="form-group">
                     <input type="submit" class="btn btn-primary" value="Salva le modifiche">
