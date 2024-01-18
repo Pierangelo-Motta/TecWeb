394a0e6908f3b6cb97ad28f1e2afabf9110c4db5
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2024-01-18 23:33:06 +0100

    gestioneMedagliere fixes

--- gestioneMedaglieri.php
+++ gestioneMedaglieri.php
@@ -3,6 +3,7 @@ session_start();
 require_once 'include/login.model.php';
 require_once 'include/config.php';
 require_once 'include/medaglieri.php';
+require_once 'include/libri.php';
 
 if (!isset($_SESSION['loggedin']) || !$_SESSION['loggedin']) {
     // Utente non autenticato, reindirizza alla pagina di login
@@ -31,8 +32,8 @@ if(isset($_POST['aggiungiMedagliere'])){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/landingPage.css">
-    <link rel="stylesheet" type="text/css" href="css/gestioneMedalglieri.css">
-    
+    <link rel="stylesheet" type="text/css" href="css/gestioneMedaglieri.css">
+
     <!-- DataTables JS -->
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js">
     </script>
@@ -42,7 +43,7 @@ if(isset($_POST['aggiungiMedagliere'])){
     <?php require('navbarSelect.php'); ?>
 
     <div class="row">
-        <div class="col-md-12 text-center m-4">
+        <div class="col-md-12 text-center m-1">
             <h1>Medaglieri</h1>
         </div>
     </div>
@@ -56,28 +57,18 @@ if(isset($_POST['aggiungiMedagliere'])){
 
     <main class="container">
         <!-- parte alta -->
-        <!-- <div class="row">
-            <div class="col-md-2"></div>
-            <div class="col-md-4  text-center">
-                <h2>Medaglieri</h2>
-            </div>
-            <div class="col-md-4  text-center">
-                <h2>Libri</h2>
-            </div>
-            <div class="col-md-2"></div>
-        </div> -->
 
         <!-- pulsante per aggiungere medaglieri -->
         <div class="row">
-            <div class="col-md-2"></div>
-            <div class="col-md-4 text-center">
+            <div class="col-md-1"></div>
+            <div class="col-md-5 text-center">
                 <button type="button" class="btn btn-primary mb-3" data-bs-toggle="modal"
                     data-bs-target="#addMedagliere">
                     Aggiungi un nuovo Medagliere
                 </button>
             </div>
-            <div class="col-md-4"></div>
-            <div class="col-md-2"></div>
+            <div class="col-md-5"></div>
+            <div class="col-md-1"></div>
         </div>
 
         <!-- Modal aggiungi Medagliere -->
@@ -106,21 +97,21 @@ if(isset($_POST['aggiungiMedagliere'])){
         <div class="container">
             <div class="row">
                 <!-- left column -->
-                <div class="col-md-2"></div>
+                <div class="col-md-1"></div>
                 <!-- medaglieri dropdown menu -->
-                <div class="col-md-4 justify-content-center">
+                <div class="col-md-5 justify-content-center">
                     <div class="dropdown">
                         <button type="button" id="dropdownButton" class="btn btn-primary dropdown-toggle"
                             data-bs-toggle="dropdown">
                             Medaglieri
                         </button>
                         <p></p>
+                        <!-- FIXME: manca l'id del medagliere -->
                         <?php $medaglieriResult = $medaglieri->getmedaglieri();?>
                         <ul class="dropdown-menu" id="dropdownMenu">
                             <?php foreach($medaglieriResult as $medagliere): ?>
                             <li>
-                                <a class="dropdown-item" id="medagliere" href="#"
-                                    id = "<?php echo $medagliere['id'];?>"
+                                <a class="dropdown-item" id="medagliere" href="#" id="<?php echo $medagliere['id'];?>"
                                     titolo="<?php echo $medagliere['titolo'];?>"
                                     descrizione="<?php echo $medagliere['descrizione'];?>">
                                     <?php echo $medagliere["titolo"] ?> </a>
@@ -128,46 +119,82 @@ if(isset($_POST['aggiungiMedagliere'])){
                             <?php endforeach; ?>
                         </ul>
 
-                    <form action="" method="post">
-                        <div class="card">
-                            <div class="card-body">
-                                <p>Titolo</p>
-                                <input type="text" class="flexible-input titolo" id="selectedItemTitoloInput" name="selectedItemTitoloInput">
+                        <form action="" method="post">
+                            <div class="card">
+                                <div class="card-body">
+                                    <p>Titolo</p>
+                                    <input type="text" class="flexible-input titolo" id="selectedItemTitoloInput"
+                                        name="selectedItemTitoloInput">
 
-                                
-                                <p>Descrizione</p>
-                                <textarea type="text" class="flexible-input descrizione" rows="4" cols="50" id="selectedItemDescrizioneInput" name="selectedItemDescrizioneInput"></textarea>
-                                
+
+                                    <p>Descrizione</p>
+                                    <textarea type="text" class="flexible-input descrizione" rows="4" cols="50"
+                                        id="selectedItemDescrizioneInput"
+                                        name="selectedItemDescrizioneInput"></textarea>
+
+                                </div>
                             </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-body">
-                                <p>Libri</p>
-                                <ul class="list-group">
-                                    <li class="list-group-item">
-                                        <button class="btn btn-danger btn-sm m-2 removebtn" id="btn_1">X</button>
-                                    aaaaa
-                                    </li>
-                                    <li class="list-group-item">
-                                        <button class="btn btn-danger btn-sm m-2 removebtn" id="btn_2">X</button>
-                                    bbbbbbb
-                                </li>
-                                </ul>
-                                                               
+
+                            <div class="card">
+                                <div class="card-body">
+                                    <p>Libri</p>
+                                    <ul class="list-group">
+                                        <?php //$libriInMedagliere = getLibriMedaglieri($medagliereId)?>
+                                        <?php //foreach($libriInMedagliere as $libro){?>
+                                        <li class="list-group-item">
+                                            <!-- reperire correttamente l'id del libro -->
+                                            <button class="btn btn-danger btn-sm m-2 removebtn" id="btn_1">X</button>
+                                            AAA
+                                        </li>
+                                        <li class="list-group-item">
+                                            <button class="btn btn-danger btn-sm m-2 removebtn" id="btn_2">X</button>
+                                            BBB
+                                            <?php //echo $libro['titolo'] ?>
+                                        </li>
+                                        <?php //}?>
+                                    </ul>
+                                </div>
                             </div>
-                        </div>
-                    <p></p>
-                    <input type="submit" class="btn btn-primary" value="Modifica Medagliere">
-                    
-                    </form>
+                            <p></p>
+
+                            <input type="submit" class="btn btn-primary" value="Modifica Medagliere">
+
+                        </form>
 
                     </div>
                 </div>
 
                 <!-- libri -->
-                <div class="col-md-4"></div>
+                <div class="col-md-5 justify-content-center">
+                    <div>
+                        <p>Selezionare un libro per aggiungerlo al medagliere</p>
+                    </div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <table id="libriTable" class="table table-striped text-center">
+                                    <thead>
+                                        <tr>
+                                            <th>Titolo</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php $libriResult = $libri->getLibri();
+                                            while ($row = mysqli_fetch_assoc($libriResult)) {?>
+                                        <tr>
+                                            <td> <?php echo $row['titolo']; ?> </td>
+                                        </tr>
+                                        <?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
                 <!-- right column -->
-                <div class="col-md-2"></div>
+                <div class="col-md-1"></div>
 
                 <!-- parte centrale libri -->
 
@@ -188,7 +215,7 @@ if(isset($_POST['aggiungiMedagliere'])){
     </script>
 
     <script src="javascript/gestioneMedaglieri.js"></script>
-
+    <script src="javascript/libri.js"></script>
 
 </body>
 
