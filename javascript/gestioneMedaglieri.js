8a317a90c88aca2c8c01527d2533b812abcd2b9f
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2024-01-19 14:48:17 +0100

    added removing book item from list

--- javascript/gestioneMedaglieri.js
+++ javascript/gestioneMedaglieri.js
@@ -77,26 +77,44 @@ $('#dropdownMenu a').on('click', function () {
 //         .catch(error => console.error('Error:', error));
 // })
 
+// Clear existing content before adding new list items
 function addLibriToList(libriInMedagliere) {
     let libriList = document.getElementById("libriList");
-
+    
     // Clear existing content before adding new list items
     libriList.innerHTML = '';
     
     libriInMedagliere.forEach(function (libro) {
         let listItem = document.createElement("li");
         listItem.className = "list-group-item";
-
+        
         let removeButton = document.createElement("button");
         removeButton.className = "btn btn-danger btn-sm m-2 removebtn";
         removeButton.textContent = "X";
         removeButton.id = "btn_" + libro.id;
-
+        
         let bookTitle = document.createTextNode(libro.titolo);
-
+        
         listItem.appendChild(removeButton);
         listItem.appendChild(bookTitle);
-
+        
         libriList.appendChild(listItem);
     });
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Event listener for the "X" buttons
+    document.getElementById('libriList').addEventListener('click', function (event) {
+        if (event.target.classList.contains('removebtn')) {
+            // Check if the clicked element has the 'removebtn' class
+            // If yes, it's the "X" button
+
+            // Get the corresponding list item
+            let listItem = event.target.closest('.list-group-item');
+
+            // Remove the list item from the list
+            listItem.remove();
+        }
+    });
+
+});
