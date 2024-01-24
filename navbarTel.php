bc4ed204d659f3c584eab67e8206ab3bf44a3d82
Luca Bandiera <lucabandiera2302@gmail.com>
2024-01-24 18:04:58 +0100

    Modify navbar for smartphone

--- navbarTel.php
+++ navbarTel.php
@@ -1,45 +1,67 @@
+<?php
+include_once("include/login.controller.php");
+include_once("include/login.model.php");
+include_once("include/model/insertOnDB.php");
+
+$userID = isset($_SESSION["userID"]) ? $_SESSION["userID"] : null;
+
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
+    $notifications = getNotifications($userID);
+}
+?>
+
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+  <script src="javascript/update_notifications.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/navbarTel.css">
+
+
 <header>
-  <!-- <nav class="navbar navbar-dark bg-dark fixed-top">
-    <div class="container-fluid">
-      <a href="landingPage.php"> <img id="logo" src="images/logoLetturePremiateSmall.jpg" alt="Logo con libro e medaglia"> </a>
-      <form action="searchingPage.php" class="d-flex me-auto">
-          Utilizzo dell'icona di ricerca come input 
-          <label for="search" class="visually-hidden">Search</label>
-          <input id="search" class="form-control" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-primary" type="submit">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-              </svg>
-          </button>
-      </form>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-        <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Letture Premiate</h5>
-          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="landingPage.php">
+                <img id="logo" src="images/logoLetturePremiateSmall.jpg" alt="Logo con libro e medaglia">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+              <br />
+                <form action="searchingPage.php" method="post" class="d-flex me-auto">
+                    <label for="searchingText" hidden>Cerca</label>
+                    <input name="searchingText" id="searchingText" class="form-control me-2" type="search" placeholder="Cerca" aria-label="Cerca" />
+                    <button class="btn btn-primary" type="submit">Cerca</button>
+                </form>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="settingPage.php">Impostazioni</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-expanded="false">Notifiche</a>
+                        <ul id="notification-list" class="dropdown-menu">
+                            <?php
+                            foreach ($notifications as $notification) {
+                                $followerId = $notification["utenteIdPost"];
+                                $followerUsername = getUserName($followerId, $conn);
+                                $notificationMessage = "$followerUsername ha iniziato a seguirti!";
+                                echo '<li><a class="dropdown-item" href="#">' . $notificationMessage . '</a></li>';
+                            }
+                            ?>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="discoveryPage.php">Discovery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="profilePage.php">Profilo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="logout.php">Logout</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-        <div class="offcanvas-body">
-          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-            <li class="nav-item">
-              <a href="settingPage.php">Impostazioni</a>
-            </li>
-            <li class="nav-item">
-              <a>Notifiche</a>
-            </li>
-            <li class="nav-item">
-              <a href="discoveryPage.php">Discovery</a>
-            </li>
-            <li class="nav-item">
-              <a href="profilePage.php">Profilo</a>
-          </li>
-          <li class="nav-item">
-            <a href="logout.php">Logout</a>
-          </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </nav> -->
+    </nav>
 </header>
