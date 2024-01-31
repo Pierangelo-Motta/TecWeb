9c459b3bfe8a790e56aa895047c8038223e7c581
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2024-01-31 10:27:36 +0100

    corrette varioe segnalazioni errori/warning js dalla console

--- profilePage.php
+++ profilePage.php
@@ -72,8 +72,8 @@ if ($user_id !== null) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>profile page</title>
-
+  <title>Profile page</title>
+ <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
--- settingPage.js
+++ settingPage.js
@@ -21,28 +21,62 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     // Click event for "Gestisci Libri"
-    document.querySelector('.book').addEventListener('click', function () {
-        hideAllContent();
+    let book = document.querySelector('.book');
+    if (book) {
+        book.addEventListener('click', function () {
+            hideAllContent();
         document.getElementById('bookContent').style.display = 'block';
-    });
+    });}
+    // questo codice, lancia un'eccezione se non  sono admin (book non esiste sulla pagina)
+    // document.querySelector('.book').addEventListener('click', function () {
+    //     hideAllContent();
+    //     document.getElementById('bookContent').style.display = 'block';
+    // });
 
     // Click event for "Gestisci Medagliere"
-    document.querySelector('.goal').addEventListener('click', function () {
-        hideAllContent();
-        document.getElementById('goalContent').style.display = 'block';
-    });
+    let goal = document.querySelector('.goal');
+    if (goal) {
+        goal.addEventListener('click', function () {
+            hideAllContent();
+            document.getElementById('goalContent').style.display = 'block';
+        });
+    }
+    // questo codice, lancia un'eccezione se non  sono admin (goal non esiste sulla pagina)
+    // document.querySelector('.goal').addEventListener('click', function () {
+    //     hideAllContent();
+    //     document.getElementById('goalContent').style.display = 'block';
+    // });
 
+    
     // Click event for "Elimina account"
-    document.querySelector('.delete').addEventListener('click', function () {
-        hideAllContent();
-        document.getElementById('deleteContent').style.display = 'block';
-    });
+    let deleteUser = document.querySelector('.delete');
+    if (deleteUser) {
+        deleteUser.addEventListener('click', function () {
+            hideAllContent();
+            document.getElementById('deleteContent').style.display = 'block';
+        });
+
+    }
+    // questo codice, lancia un'eccezione se non  sono admin (delete non esiste sulla pagina)
+    // document.querySelector('.delete').addEventListener('click', function () {
+    //     hideAllContent();
+    //     document.getElementById('deleteContent').style.display = 'block';
+    // });
+
 
     // Click event for "Cambia password"
-    document.querySelector('.manage').addEventListener('click', function () {
-        hideAllContent();
-        document.getElementById('manageContent').style.display = 'block';
-    });
+    let manage = document.querySelector('.manage');
+    if (manage) {
+        manage.addEventListener('click', function () {
+            hideAllContent();
+            document.getElementById('manageContent').style.display = 'block';
+        });
+    }
+    // questo codice, lancia un'eccezione se non  sono admin (manage non esiste sulla pagina)
+    // document.querySelector('.manage').addEventListener('click', function () {
+    //     hideAllContent();
+    //     document.getElementById('manageContent').style.display = 'block';
+    // });
 
     function hideAllContent() {
         // Hide all content divs
