dde61cde7b3bf0666fd5d8243664bd72cbfcf810
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2024-01-31 09:36:24 +0100

    index:  accessibility corrections

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
                             <div class="form-group row">
                                 <label class="col-sm-2 col-form-label col-form-label-sm" for="username">Username:</label>
                                 <div class="col-sm-10">
-                                    <input class="form-control" type="text" id="username" name="username" required>
+                                    <input class="form-control" type="text" id="username" name="username" required autocomplete="username">
                                 </div>
                             </div>
                             
