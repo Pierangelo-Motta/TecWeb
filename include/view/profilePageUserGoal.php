e76492cadf1b2ab620c73604ae0c9c635a91bcc4
Jacopo Pesaresi <jacopo.pesaresi2@studio.unibo.it>
2024-02-01 22:55:40 +0100

    ciao

--- include/view/profilePageUserGoal.php
+++ include/view/profilePageUserGoal.php
@@ -7,10 +7,13 @@ require("include/view/bookSummoner.php");
 
 <section id="userGoal">
     <!-- <div class="col-1">  </div> -->
-
+    <?php if ($_SESSION["id"] != 7) : ?>
     <div class="book">
         <?php echo createBook($medIndex, $amountComplete); ?>
     </div>
+    <?php else : ?>
+        <h3>Utente admin: non possiede librone! 😉</h3>
+    <?php endif; ?>
 
     <!-- <div class="col-1">  </div> -->
 </section>
