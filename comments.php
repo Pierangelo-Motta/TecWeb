efc0c3d209edc947bdbb1d6e87b017af38e56d7d
bandieralucaa <lucabandiera2302@gmail.com>
2024-02-07 16:13:34 +0100

    Change title in comments

--- comments.php
+++ comments.php
@@ -32,7 +32,7 @@ if(!isset($_GET["userIdPost"]) || !isset($_GET["timePost"]) || !checkIfPostExist
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>New post</title>
+    <title>TecWeb - Commenta</title>
 
 
     <!-- Bootstrap CSS -->
